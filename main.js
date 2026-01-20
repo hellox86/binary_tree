0c55734944813f17af1cc9424d766a879b5f3b75
hellox86 <zvonkov.dalv@gmail.com>
2026-01-20 21:30:59 +0800

    base operations 2, todo: fix delete

--- main.js
+++ main.js
@@ -35,16 +35,15 @@ function add_el_to_tree(r, data) {
   }
 }
 
-function min_in_tree(r, data = -1) {
-  if (r != null) {
-    return min_in_tree(r.left, r.data);
-  } else {
-    return data;
-  }
+//
+function min_in_tree(r) {
+  if (r.left.left == null) return r;
+  return min_in_tree(r.left);
 }
 // Помогло с функцией удаления: https://ru.stackoverflow.com/questions/660940/%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8%D0%B7-%D0%B1%D0%B8%D0%BD%D0%B0%D1%80%D0%BD%D0%BE%D0%B3%D0%BE-%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%B0
 
 function remove_el_from_tree(r, data, parent) {
+  debugger;
   if (r != null) {
     if (data > r.data) {
       return remove_el_from_tree(r.right, data, r);
@@ -73,18 +72,22 @@ function remove_el_from_tree(r, data, parent) {
         r.left = toRemove.left;
         r.right = toRemove.right;
       } else if (r.left != null && r.right != null) {
-        if (r.right.left != null) {
-          r.data = min_in_tree(r.right);
-        } else {
+        if (r.right.left == null) {
           r.data = r.right.data;
+
+          let rr = r.right.right;
           r.right = null;
-          r.right = r.right.right;
+          r.right = rr;
+        } else {
+          let res = min_in_tree(r.right);
+          r.data = res.left.data;
+          res.left = null;
         }
       }
     }
   }
 }
-
+// TEST
 init_tree(14);
 add_el_to_tree(root, 1);
 add_el_to_tree(root, 653);
@@ -103,62 +106,89 @@ add_el_to_tree(root, 541);
 add_el_to_tree(root, 96);
 add_el_to_tree(root, 6);
 
-// function get_count(r, c = 0) {
+// TEST 2
+// init_tree(0);
+
+// for (let i = 1; i <= 100; i++) {
+//   add_el_to_tree(root, i);
+// }
+
+// function isLeaf(r) {
+//   if (r == null) return true;
+//   if (r.left == null && r.right == null) {
+//     return true;
+//   }
+//   return false;
+// }
+// let flag = false;
+
+// my strange version of print tree
+// function iterate(r, parent) {
 //   if (r != null) {
+//     console.log(r.data);
 //     if (r.left != null) {
-//       return get_count(r.left, c + 1);
+//       if (r.right != null && !flag) {
+//         console.log(r.right.data);
+//       }
+//       return iterate(r.left, r);
+//     } else if (r.right != null) {
+//       if (r.left != null && !flag) {
+//         console.log(r.left.data);
+//       }
+//       return iterate(r.right, r);
 //     } else {
-//       return get_count(r.right, c + 1);
+//       if (parent.left === r && !isLeaf(parent.right)) {
+//         flag = true;
+//         return iterate(parent.right, parent);
+//       } else if (parent.right === r && !isLeaf(parent.left)) {
+//         flag = true;
+//         return iterate(parent.left, parent);
+//       }
 //     }
-//   } else {
-//     return c;
 //   }
 // }
 
-function left_iterate(r) {
-  // debugger;
-  if (r != null) {
-    console.log(r.data);
-    if (r.right != null) {
-      console.log(r.right.data);
-    }
-    return left_iterate(r.left);
-  } else {
-    if (r.right != null) {
-      return left_iterate(r.right);
-    }
+function show_tree(r) {
+  console.log(root.data);
+  const cur = r;
+  if (r.left != null) {
+    r = r.left;
+    iterate(r, r);
   }
-}
-function right_iterate(r) {
-  if (r != null) {
-    console.log(r.data);
-    if (r.left != null) {
-      console.log(r.left.data);
-    }
-    return left_iterate(r.right);
+
+  if (cur.right != null) {
+    r = cur.right;
+    iterate(r, r);
   }
 }
 
-function print(r) {
-  // debugger;
-  if (r != null) {
-    if (r.left != null) {
-      return print(r.left);
-    } else if (r.right != null) {
-      return print(r.right);
-    }
-  }
-}
+// default print tree
 
 function show_tree(r) {
-  console.log(r.data);
-  const cur = r;
-  r = r.left;
-  left_iterate(r);
-  r = cur.right;
-  right_iterate(r);
+  let c = 0;
+
+  function iterate(r) {
+    if (r != null) {
+      iterate(r.left);
+      c++;
+      console.log(r.data);
+      iterate(r.right);
+    }
+  }
+  iterate(root);
+
+  return c;
 }
 console.log(root);
-show_tree(root);
 
-//remove_el_from_tree(root, 14, root);
+let c = show_tree(root);
+
+console.log(" ");
+console.log(c);
+console.log(" ");
+remove_el_from_tree(root, 1, root);
+c = show_tree(root);
+console.log(c);
+// console.log(" ");
+
+// show_tree(root);
